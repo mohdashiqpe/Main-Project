34fd440f8bb5b4beee75c4427e0a770011c7a944
mohdashiqpe <119798553+mohdashiqpe@users.noreply.github.com>
2024-03-15 22:59:09 +0530

    Old Project

--- Source/UsedStore/static/js/purpose build/formCaller12.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    newPasswordInput.addEventListener("input", validatePassword);
-    confirmPasswordInput.addEventListener("input", validateConfirmPassword)
-});
--- Source/UsedStore/static/js/purpose build/form_Caller3.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-    regForm3.addEventListener("submit", function (event){
-        event.preventDefault();
-    });
-    otpNum.addEventListener("input", handleOtp);
-});
-
